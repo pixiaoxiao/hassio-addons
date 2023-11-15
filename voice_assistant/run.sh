0b2b4111f9013d486f698a8aef6ef1d98dd47d87
pixiaoxiao <123456>
2023-11-15 17:23:24 +0800

    del other add-ons

--- voice_assistant/run.sh
+++ voice_assistant/run.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/with-contenv bashio
+sleep 5
+if [ ! -e /share/voice_assistant ]; then
+    mkdir -p /share/voice_assistant
+    cp /process_programs/* /share/voice_assistant
+fi
 
-#if [ ! -e /share/voice_assistant ]; then
-#    mkdir -p /share/voice_assistant
-#    cp /process_programs/* /share/voice_assistant
-#fi
-#
-#if [ ! -e /media/voice_assistant ]; then
-#    mkdir -p /media/voice_assistant
-#    cp /audio/* /media/voice_assistant/
-#fi
+if [ ! -e /media/voice_assistant ]; then
+    mkdir -p /media/voice_assistant
+    cp /audio/* /media/voice_assistant/
+fi
 
-#python3 run.py 2>&1
+python3 run.py 2>&1
 
-while [[ true ]]; do
-    sleep 1
-done
+#while [[ true ]]; do
+#    sleep 1
+#done
